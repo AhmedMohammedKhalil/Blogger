9a49b40fe9f59a5134157d5d8e9f6903ff24113b
ProgAhmedKhalil <a.m.k.y.2013@gmail.com>
2021-10-24 16:41:24 +0200

    initial commit

---
+++ database/migrations/2021_10_24_024844_create_reactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('by')->unsigned();
+            $table->foreign('by')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->string('reactionable_type');
+            $table->integer('reactionable_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reactions');
+    }
+}
